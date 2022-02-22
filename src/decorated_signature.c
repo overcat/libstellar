3d6321da16ed2a8ac8e56fac464ddf5f711130c1
Jun Luo <4catcode@gmail.com>
2022-02-22 21:06:31 +0800

    feat: decorated signature and test cases

--- src/decorated_signature.c
+++ src/decorated_signature.c
@@ -2,14 +2,16 @@
 
 bool decorated_signature_to_xdr_object(const struct DecoratedSignature *in,
                                        stellarxdr_DecoratedSignature *out) {
-  memcpy(out->hint, in->signatureHint);
-  memcpy(out->signature, in->signature);
+  memcpy(out->hint, in->signatureHint, 4);
+  out->signature.stellarxdr_Signature_len = 64;
+  out->signature.stellarxdr_Signature_val = malloc(64);
+  memcpy(out->signature.stellarxdr_Signature_val, in->signature, 64);
   return true;
 };
 
 bool decorated_signature_from_xdr_object(
     const stellarxdr_DecoratedSignature *in, struct DecoratedSignature *out) {
-  memcpy(out->signatureHint, in->hint);
-  memcpy(out->signature, in->signature);
+  memcpy(out->signatureHint, in->hint, 4);
+  memcpy(out->signature, in->signature.stellarxdr_Signature_val, 64);
   return true;
 };
--- src/decorated_signature.h
+++ src/decorated_signature.h
@@ -11,8 +11,8 @@ struct DecoratedSignature {
 };
 
 bool decorated_signature_to_xdr_object(const struct DecoratedSignature *in,
-                                 stellarxdr_DecoratedSignature *out);
+                                       stellarxdr_DecoratedSignature *out);
 
-bool decorated_signature_from_xdr_object(const stellarxdr_DecoratedSignature *in,
-                                   struct DecoratedSignature *out);
+bool decorated_signature_from_xdr_object(
+    const stellarxdr_DecoratedSignature *in, struct DecoratedSignature *out);
 #endif // DECORATED_SIGNATURE_H
