368aa18e2aa6af17040cae678b74bf64ca0c3531
Jun Luo <4catcode@gmail.com>
2022-03-15 22:41:04 +0800

    feat: trust_line_asset and change_trust_asset

--- src/asset.h
+++ src/asset.h
@@ -23,4 +23,16 @@ bool asset_to_xdr_object(const struct Asset *in, stellarxdr_Asset *out);
 
 bool asset_from_xdr_object(const stellarxdr_Asset *in, struct Asset *out);
 
+bool asset_to_trust_line_asset_xdr_object(const struct Asset *in,
+                                          stellarxdr_TrustLineAsset *out);
+
+bool asset_from_trust_line_asset_xdr_object(const stellarxdr_TrustLineAsset *in,
+                                            struct Asset *out);
+
+bool asset_to_change_trust_asset_xdr_object(const struct Asset *in,
+                                            stellarxdr_ChangeTrustAsset *out);
+
+bool asset_from_change_trust_asset_xdr_object(
+    const stellarxdr_ChangeTrustAsset *in, struct Asset *out);
+
 #endif
